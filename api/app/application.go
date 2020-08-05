899f14c06e245cda6c95992f7d588be11377a9d8
sug5806 <sug5806@gmail.com>
2020-08-05 22:57:09 +0900

    실행 함수 추가

---
+++ api/app/application.go
@@ -0,0 +1,13 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+var router *gin.Engine
+
+func init() {
+	router = gin.Default()
+}
+
+func StartApp() {
+	url()
+}
---
+++ api/app/url.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"golang_micro_service_practice/api/controller/hello"
+	"golang_micro_service_practice/api/controller/repositories"
+)
+
+func url() {
+	router.GET("/", hello.Hello)
+	router.POST("/create_repo", repositories.CreateRepo)
+}
